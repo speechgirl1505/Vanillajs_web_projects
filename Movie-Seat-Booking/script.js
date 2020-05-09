361786e5dd3810a9fec1b3c1449d3120e0cf93c3
Kala Elam <speechgirl1505@gmail.com>
2020-05-08 23:46:19 -0500

    added local storage save

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -10,6 +10,12 @@ let ticketPrice = +movieSelect.value;
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  console.log(seatsIndex);
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
