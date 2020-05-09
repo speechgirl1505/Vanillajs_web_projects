cc50e2a3c6568d188aa968dff0d4563aca48da6d
Kala Elam <speechgirl1505@gmail.com>
2020-05-09 12:20:10 -0500

    update count/total and get from local storage

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -4,7 +4,15 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+//Save Selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update total and count
 function updateSelectedCount() {
@@ -23,9 +31,29 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//GEt data from local storage and populate ui
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -40,3 +68,6 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 });
+
+//Initial count and total set
+updateSelectedCount();
